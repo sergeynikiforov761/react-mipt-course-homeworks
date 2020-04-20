5054ddba3bef085287e28bdbdde97220107508e2
sbt-milov-av <avmilov.sbt@sberbank.ru>
2020-04-20 21:28:34 +0300

    Начал с оформления. Пока на этом и закончил :(

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import d from './Dashboard.module.css'
+export const Dashboard = () => {
+    return <div className={d.content}>
+        DashBoard wtf?
+    </div>
+};
+
+export default Dashboard;
