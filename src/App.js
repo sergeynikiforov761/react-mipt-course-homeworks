37b86ac96b814afe9e4c4ecf413639b403e54b0a
Andrey <andrew-fi@yandex.ru>
2020-04-30 01:38:20 +0300

    first two tasks

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import {Login} from "./Login"
+import {Dashboard} from "./Dashboard";
+import {Register} from "./Register";
+
 
 function App() {
+    const auth = localStorage.getItem('AUTH');
     return (
         <div>
-            This homework requires a Hero!
+            <h2>
+                My App
+            </h2>
+            <Router>
+                {!auth && <Switch>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Redirect to="/login"/>
+                </Switch>}
+                {!!auth && <Switch>
+                    <Route exact path="/" component={Dashboard}/>
+                    <Redirect to="/"/>
+                </Switch>}
+            </Router>
         </div>
     );
 }
